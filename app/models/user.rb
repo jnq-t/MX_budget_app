312045504b82a6d18d7754360dd6855518ee5f22
jnq-t <jessequebbeman@gmail.com>
2021-04-11 16:32:42 -0700

    created update members method for user model, added patch to create members method for user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -115,24 +115,28 @@ class User < ApplicationRecord
       #POST create new member
       post_response = HTTP.headers(:accept => @@accept, :'content-type' => @@content_type).basic_auth(:user => ENV["API_USERNAME"], :pass => ENV["API_PASSWORD"])
       .post("#{@@base_url}/users/#{self.guid}/members", :json => { :member => { :credentials=> credentials_array,
-                                                                   :institution_code => institution_code} }).parse["member"]
-
+                                                                   :institution_code => institution_code} })
       #create member in db
-      if post_response 
+      if post_response.code == 202
+        post_response = post_response.parse["member"]
+        #TODO update with passing hash
         Member.create(guid: post_response["guid"], member_id: post_response["id"], user_guid: post_response["user_guid"], 
           aggregated_at: post_response["aggregated_at"], institution_code: post_response["institution_code"], 
           is_being_aggregated: post_response["is_being_aggregated"], is_oauth: post_response["is_oauth"], 
           metadata: post_response["metadata"], name: post_response["name"], successfully_aggregated_at: post_response["successfully_aggregated_at"])  
-      else
-        return "error with post response"
-      end
+      
+      elsif post_response.code == 404
+        #404 eror patch
+        self.update_members
+        return self.find_members
 
-    else 
-      return "error with credentials response"
+      else  
+        return post_response.code
+      end
     end
 
       
-    end
+  end
 
     #list members associated with user in API
     def list_members
@@ -144,5 +148,68 @@ class User < ApplicationRecord
   def find_members 
     Member.where(:user_guid => self.guid)
   end
+
+  # def update_members_db
+  #   response = self.list_members
+  #     response.each do |member|
+  #       member_db = Member.find_by member["guid"]
+  #       if member_db
+  #         #UPDATE MEMBER WITH RELEVANT FIELDS
+  #       else
+  #         #CREATE MEMBER WITH RELEVANT FIELDS
+  #         Member.create(
+  # def update_members_db
+  #   self.list_members.each do |api_member|
+  #     member_params = {}
+  #     api_member.each_pair do |key, value|
+  #       if Member.attribute_names.include? key
+  #         unless key == "id" 
+  #           member_params[key] = value
+  #         end
+  #       end
+  #       puts member_params
+  #       new_member = Member.new(member_params)
+  #       if new_member.valid?
+  #         new_member.save
+  #       else
+  #         return "invalid member params" 
+  #       end
+  #     end
+  #   end
+  # end
+
+  def update_members
+    #create new members
+    self.list_members.each do |api_member|
+      member_params = {}
+      api_member.each_pair do |key, value|
+        unless key == "id"
+          if Member.attribute_names.include? key
+            # puts " #{key}: #{value}"
+            member_params[key] = value
+          end
+        end
+      end
+      #if member already exists
+      temp = Member.find_by guid: member_params["guid"]
+      if !!temp
+        temp.attributes = member_params
+        if temp.valid? 
+          temp.save
+        else
+          return "problem updating existing member"
+        end
+      else
+        member = Member.new(member_params)
+        if member.valid?
+          member.save
+        else
+          return "problem with member params"
+        end
+      end
+    end
+  end
+
+
 
 end
