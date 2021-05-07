4bac6b1455700028edb0012492490d70f694648b
jnq-t <jessequebbeman@gmail.com>
2021-05-07 11:35:01 -0700

    Revert "moved check status persistent function from users to members"
    This reverts commit 258010a2e433e48676bed64597a10e3091cad950.

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -11,7 +11,7 @@ class MembersController < ApplicationController
     begin
     member = current_user.create_member(params[:member][:institution_code], params[:member][:username], params[:member][:password])
     if !!member 
-      status = member.check_status_persistent 
+      status = current_user.check_status_persistent(member.guid)
     end
     rescue
       flash.now[:danger] = "Trouble connecting with MX api, try again!" 
